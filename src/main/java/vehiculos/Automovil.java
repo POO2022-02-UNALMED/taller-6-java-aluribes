9f25e1d51c211d0b0cf9b2a9ec4df5a3936474d8
Alejandra Uribe Sierra <aluribes@unal.edu.co>
2022-10-04 03:25:05 -0500

    Create Automovil.java

---
+++ src/main/java/vehiculos/Automovil.java
@@ -0,0 +1,23 @@
+package vehiculos;
+
+public class Automovil extends Vehiculo{
+
+	private int puestos;
+
+	// Constructor
+	public Automovil(String placa, String nombre, int precio, int peso, Fabricante fabricante, int puestos) {
+		super(placa, 4, 100, nombre, precio, peso, "FWD", fabricante);
+		this.puestos = puestos;
+		Vehiculo.cantAutomovil++;
+	}
+
+	// Set y Get
+
+	public void setPuestos(int puestos) {
+		this.puestos = puestos;
+	}
+
+	public int getPuestos() {
+		return puestos;
+	}
+}
