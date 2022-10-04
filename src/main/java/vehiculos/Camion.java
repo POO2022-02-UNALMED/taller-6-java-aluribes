cdbdc1b5230cc060fe9134f4b40f0760938c0738
Alejandra Uribe Sierra <aluribes@unal.edu.co>
2022-10-04 03:25:10 -0500

    Create Camion.java

---
+++ src/main/java/vehiculos/Camion.java
@@ -0,0 +1,22 @@
+package vehiculos;
+
+public class Camion extends Vehiculo{
+
+	private int ejes;
+
+	// Constructor
+	public Camion(String placa, String nombre, int precio, int peso, Fabricante fabricante, int ejes) {
+		super(placa, 2, 80, nombre, precio, peso, "4X2", fabricante);
+		this.ejes = ejes;
+		Vehiculo.cantCamion++;
+	}
+
+	// Sey y Get
+	public void setEjes(int ejes) {
+		this.ejes = ejes;
+	}
+	
+	public int getEjes() {
+		return ejes;
+	}
+}
